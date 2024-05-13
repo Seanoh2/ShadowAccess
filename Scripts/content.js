98fa39b148214a641aba598e4b0f634560b0119f
Sean O'Hora <seanoh2@live.com>
2024-05-13 11:23:28 +0100

    Added basic implementation
    Need to go look more into cases to confirm  p search will work

--- Scripts/content.js
+++ Scripts/content.js
@@ -1,7 +1,28 @@
-console.log("Testing")
 const tickets = document.getElementsByClassName("zd-comment");
+const username = "";
+const password = "";
 
-const observer = new MutationObserver();
+function callback(mutationList) {
+    mutationList.forEach((mutation) => {
+        var lines = mutation.getElementsByTagName("p");
+        for(var line in lines) {
+            var usernameReg = new RegExp("Username: [a-zA-Z0-9]*$");
+            var passwordReg = new RegExp("Password: [a-zA-Z0-9]*$");
+
+            if(usernameReg.test(line)) {
+                username = line.replace("Username: ", "");
+                console.log("Username Found: " + username);
+            }
+
+            if(passwordReg.test(line)) {
+                password = line.replace("Password: ", "");
+                console.log("Password Found: " + password)
+            }
+        }
+    });
+  }
+
+const observer = new MutationObserver(callback);
 observer.observe(tickets, {
     subtree: true,
     childList: true,
